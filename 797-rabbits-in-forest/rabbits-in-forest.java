66f2f7f06e97fdaa4798a2a3213c940003a4cf07
MouleeswaranR <moulee078@gmail.com>
2025-04-20 16:20:26 +0530

    Time: 4 ms (30.84%) | Memory: 42.2 MB (81.02%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -4,8 +4,13 @@ class Solution {
         for (int i : answers) mpp.put(i, mpp.getOrDefault(i, 0) + 1);
 
         int total = 0;
-        for (Map.Entry<Integer, Integer> p : mpp.entrySet())
-            total += Math.ceil((double)p.getValue() / (p.getKey() + 1)) * (p.getKey() + 1);
+        for (Map.Entry<Integer, Integer> p : mpp.entrySet()){
+            // System.out.println(p.getKey());
+            // System.out.println(p.getValue());
+             total += Math.ceil((double)p.getValue() / (p.getKey() + 1)) * (p.getKey() + 1);
+        }
+        
+           
 
         return total;
     }
