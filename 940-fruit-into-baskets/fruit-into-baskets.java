fca15fd7636f40d0a7b39def492f7b88f2f88d6e
MouleeswaranR <moulee078@gmail.com>
2025-10-19 09:37:48 +0530

    Time: 53 ms (24.40%) | Memory: 55.5 MB (32.04%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -4,7 +4,7 @@ class Solution {
       Map<Integer,Integer> mpp=new HashMap<>();
       while(right<fruits.length){
         mpp.put(fruits[right],mpp.getOrDefault(fruits[right],0)+1);
-        while(mpp.size()>2){
+        if(mpp.size()>2){
             mpp.put(fruits[left],mpp.get(fruits[left])-1);
             if(mpp.get(fruits[left])==0)mpp.remove(fruits[left]);
             left++;
