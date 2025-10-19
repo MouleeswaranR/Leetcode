56909b815ec7a9c81aa2f947b60c803c7bd87b4b
MouleeswaranR <moulee078@gmail.com>
2025-10-19 09:37:36 +0530

    Time: 55 ms (14.71%) | Memory: 55.3 MB (41.33%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,29 +1,49 @@
-public class Solution {
+class Solution {
     public int totalFruit(int[] fruits) {
-        int n = fruits.length;
-        int lastFruit = -1, secondLastFruit = -1;
-        int lastCount = 0, currMax = 0, max = 0;
-
-        for (int i = 0; i < n; i++) {
-            int fruit = fruits[i];
-
-            if (fruit == lastFruit || fruit == secondLastFruit) {
-                currMax++;
-            } else {
-                currMax = lastCount + 1;
-            }
-
-            if (fruit == lastFruit) {
-                lastCount++;
-            } else {
-                lastCount = 1;
-                secondLastFruit = lastFruit;
-                lastFruit = fruit;
-            }
-
-            max = Math.max(max, currMax);
+        int left=0,right=0,maxlen=0;
+      Map<Integer,Integer> mpp=new HashMap<>();
+      while(right<fruits.length){
+        mpp.put(fruits[right],mpp.getOrDefault(fruits[right],0)+1);
+        while(mpp.size()>2){
+            mpp.put(fruits[left],mpp.get(fruits[left])-1);
+            if(mpp.get(fruits[left])==0)mpp.remove(fruits[left]);
+            left++;
         }
-
-        return max;
+        if(mpp.size()<=2){
+            maxlen=Math.max(maxlen,right-left+1);
+        }
+        right++;
+      }
+      return maxlen;
     }
 }
+
+// public class Solution {
+//     public int totalFruit(int[] fruits) {
+//         int n = fruits.length;
+//         int lastFruit = -1, secondLastFruit = -1;
+//         int lastCount = 0, currMax = 0, max = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             int fruit = fruits[i];
+
+//             if (fruit == lastFruit || fruit == secondLastFruit) {
+//                 currMax++;
+//             } else {
+//                 currMax = lastCount + 1;
+//             }
+
+//             if (fruit == lastFruit) {
+//                 lastCount++;
+//             } else {
+//                 lastCount = 1;
+//                 secondLastFruit = lastFruit;
+//                 lastFruit = fruit;
+//             }
+
+//             max = Math.max(max, currMax);
+//         }
+
+//         return max;
+//     }
+// }
