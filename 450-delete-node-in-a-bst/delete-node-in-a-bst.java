a4e2bba48ac7576411cf26651f0e80f30a07c531
MouleeswaranR <moulee078@gmail.com>
2025-11-10 19:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (11.61%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null)return null;
+        if(root.val==key){
+            return helper(root);
+        }
+        TreeNode curr=root;
+        while(root!=null){
+            if(root.val>key){
+                if(root.left!=null && root.left.val==key){
+                    root.left=helper(root.left);
+                    break;
+                }else{
+                    root=root.left;
+                }
+            }else{
+                if(root.right!=null && root.right.val==key){
+                    root.right=helper(root.right);
+                    break;
+                }else{
+                    root=root.right;
+                }
+            }
+        }
+        return curr;
+    }
+
+    public TreeNode helper(TreeNode node){
+        if(node.left==null){
+            return node.right;
+        }else if(node.right==null){
+            return node.left;
+        }else{
+            TreeNode rightChild=node.right;
+            TreeNode lastRightChild=lastchild(node.left);
+            lastRightChild.right=rightChild;
+            return node.left;
+        }
+    }
+
+    public TreeNode lastchild(TreeNode node){
+        if(node.right==null)return node;
+        return lastchild(node.right);
+    }
+}
