879cc1109bbfa540fd530b969f0d914606491f24
MouleeswaranR <moulee078@gmail.com>
2026-01-16 19:09:03 +0530

    Time: 4 ms (90.63%) | Memory: 45.9 MB (92.85%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -36,24 +36,62 @@
 //Tabulation
 // //Time Complexity : O(n*2)
 // //Space Complexity: O(n)
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n=nums.length;
+//         int[] dp=new int[n];
+//         //each each index  i in dp stores the longest increasing subsequence from index o to that index i
+//         Arrays.fill(dp,1);
+        
+//         int maxLIS=1;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[i]>nums[j]){
+//                     dp[i]=Math.max(dp[i],dp[j]+1);
+//                     maxLIS=Math.max(maxLIS,dp[i]);
+//                 }
+               
+//             }
+//         }
+//         return maxLIS;
+//     }
+// }
+
+//Patience Sorting 
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[] dp=new int[n];
-        //each each index  i in dp stores the longest increasing subsequence from index o to that index i
-        Arrays.fill(dp,1);
-        
-        int maxLIS=1;
+        ArrayList<Integer> ls=new ArrayList<>();
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    maxLIS=Math.max(maxLIS,dp[i]);
-                }
-               
+
+            int index=binarySearch(ls,nums[i]);//finding the index of number just greater than the current nums[i]
+
+            if(index==ls.size()){//if index returned is size of ls means no element greater than current element is found, so just add
+                ls.add(nums[i]);
+            }else{
+                ls.set(index,nums[i]);//putting the current element at the index where the value is just greter than current element
             }
         }
-        return maxLIS;
+        return ls.size();
+    }
+
+
+    public int binarySearch(ArrayList<Integer> sorted,int target){
+        int left=0,right=sorted.size();
+        int result=sorted.size();
+
+        while(left<right){
+            int mid=left+(right-left)/2;
+
+            if(sorted.get(mid)<target){
+                left=mid+1;
+            }else{
+                result=mid;
+                right=mid;
+            }
+        }
+        return result;
     }
 }
