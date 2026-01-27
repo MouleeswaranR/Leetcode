d37d19dbd240d8d802bdd73eeb9a82c020fd289f
MouleeswaranR <moulee078@gmail.com>
2026-01-27 19:10:36 +0530

    Time: 121 ms (51.82%) | Memory: 277.1 MB (13.76%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -0,0 +1,56 @@
+class Solution {
+    static class Pair{
+        int node;
+        int weight;
+
+        public Pair(int node,int weight){
+            this.node=node;
+            this.weight=weight;
+        }
+    }
+    public int minCost(int n, int[][] edges) {
+
+        Map<Integer, List<Pair>> adj = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair(v, wt));
+            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair(u, 2 * wt)); // reversed edge
+        }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        pq.offer(new Pair(0, 0)); 
+
+        while (!pq.isEmpty()) {
+            Pair cur = pq.poll();
+            int node = cur.node;
+            int d = cur.weight;
+
+            if (d > dist[node]) continue;
+
+            if (node == n - 1) return d;
+
+            if (!adj.containsKey(node)) continue;
+
+            for (Pair nei : adj.get(node)) {
+                int nextNode = nei.node;
+                int weight = nei.weight;
+
+                if (d + weight < dist[nextNode]) {
+                    dist[nextNode] = d + weight;
+                    pq.offer(new Pair(nextNode, dist[nextNode]));
+                }
+            }
+        }
+
+        return -1;
+    }
+}
