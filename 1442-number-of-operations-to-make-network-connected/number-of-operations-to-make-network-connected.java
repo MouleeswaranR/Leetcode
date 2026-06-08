87c729f3e463d582c534e2641639fa32e4cf3876
MouleeswaranR <moulee078@gmail.com>
2026-06-08 22:16:23 +0530

    Time: 5 ms (77.20%) | Memory: 66 MB (75.42%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,47 +1,63 @@
-class DisjointSet{
-    int[] parent,size;
-    public DisjointSet(int n){
-        parent=new int[n];
-        size=new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    public int findPar(int node){
-        if(node==parent[node])return node;
-        return parent[node]=findPar(parent[node]);
-    }
-    public void unionBySize(int u,int v){
-        int ulpU=findPar(u),ulpV=findPar(v);
-        if(ulpU==ulpV)return;
-        if(size[ulpU]<size[ulpV]){
-            parent[ulpU]=ulpV;
-            size[ulpV]+=size[ulpU];
-        }else{
-            parent[ulpV]=ulpU;
-            size[ulpU]+=size[ulpV];
-        }
-    }
-}
-
 class Solution {
+    int[] parent;
+    int[] rank;
+
+    public int find(int node){
+        if(node==parent[node]){
+            return node;
+        }
+
+        return parent[node]=find(parent[node]);
+    }
+
+    public void union(int u,int v){
+        int parentU=find(u),parentV=find(v);
+
+        if(parentU==parentV){
+            return;
+        }
+
+
+        if(rank[parentU]>rank[parentV]){
+            parent[parentV]=parentU;
+        }else if(rank[parentV]>rank[parentU]){
+            parent[parentU]=parentV;
+        }else{
+            parent[parentV]=parentU;
+            parentU++;
+        }
+    }
+
     public int makeConnected(int n, int[][] connections) {
-        DisjointSet ds=new DisjointSet(n);
-        int cntExt=0;
-        for(int[] edge:connections){
-            int u=edge[0],v=edge[1];
-            if(ds.findPar(u)==ds.findPar(v)){
-                cntExt++;
-            }else{
-                ds.unionBySize(u,v);
+        
+        //minimum n-1 edges needed to connect n nodes, if there is less than n-1 nodes, return -1
+        if(connections.length<n-1){
+            return -1;  
+        }
+
+        rank=new int[n];
+        parent=new int[n];
+
+        for(int i=0;i<n;i++){
+            rank[i]=1;
+            parent[i]=i;
+        }
+
+        int components=n;//initailly each node is a component;
+
+        //go through all edges
+        for(int[] connection:connections){
+            //find parent for each edge nodes
+            int parent1=find(connection[0]),parent2=find(connection[1]);
+
+            //if parent are not same but have edge between them,connect them
+            //connecting one node to one node or component reduces a component
+            if(parent1!=parent2){
+                union(connection[0],connection[1]);//connect the nodes
+                components--;//reduce one components as 2 nodes become 1 component
             }
         }
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(i==ds.parent[i])cnt++;
-        }
-        if(cntExt>=cnt-1)return cnt-1;
-        return -1;
+
+        return components-1;//-1 because n nodes need n-1 edges to connect them
     }
 }
