d6bba8990138a2d45a7c54d85d97e09260922e30
MouleeswaranR <moulee078@gmail.com>
2026-06-11 21:47:35 +0530

    Time: 7 ms (8.79%) | Memory: 47.2 MB (56.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,22 +1,47 @@
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-     int n=graph.length;
-     int[] colors=new int[n];
-     for(int i=0;i<n;i++){
-        if(colors[i]==0){
-            if(checkValidColor(i,1,colors,graph)==false) return false;
+    public boolean checkIsBiPartite(int currNode,int currCol,int[] color,List<List<Integer>> adjList){
+        color[currNode]=currCol;//color current node
+
+        //traverse through adjacent nodes
+        for(int neighbor:adjList.get(currNode)){
+            //if neighbor is still not visited
+            if(color[neighbor]==-1){
+                //check for bipartiteness thorugh neighbor
+                if(checkIsBiPartite(neighbor,1-currCol,color,adjList)==false){
+                    return false;
+                }
+            }else  if(color[neighbor]==color[currNode]){//if color of current node and its neighbor is same, there is no bipartiteness
+                return false;
+            }
         }
-     }
-     return true;
+
+        return true;//there is bipartiteness
     }
-    public boolean checkValidColor(int node,int color,int[] colors,int[][] graph){
-        if(colors[node]!=0){
-            return colors[node]==color;
+    public boolean isBipartite(int[][] graph) {
+        List<List<Integer>> adjList=new ArrayList<>();
+        
+        int n=graph.length;
+
+        for(int i=0;i<n;i++)adjList.add(new ArrayList<>());
+        
+        //building adjacency list
+        for(int i=0;i<n;i++){
+            for(int neighbor : graph[i]){
+                adjList.get(i).add(neighbor);
+            }
         }
-        colors[node]=color;
-        for(int adjNode:graph[node]){
-            if(!checkValidColor(adjNode,-color,colors,graph)) return false;
+
+        //to keep track of color of each node
+        int[] color=new int[n];
+        Arrays.fill(color,-1);//fill initially with -1 mean not visited
+
+        //for each node, check if it is not visited and check for bipartiteness
+        for(int i=0;i<n;i++){
+            if(color[i]==-1 && checkIsBiPartite(i,1,color,adjList)==false){
+                return false;
+            }
         }
+
         return true;
     }
 }
