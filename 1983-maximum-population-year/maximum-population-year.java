e425923eaab097738e7ad062b8f93fe95f16aef4
MouleeswaranR <moulee078@gmail.com>
2026-06-23 20:02:18 +0530

    Time: 4 ms (16.93%) | Memory: 43.5 MB (61.74%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,5 +1,5 @@
 class Solution {
-    //using line sweep technique
+    //using Line sweep technique
     public int maximumPopulation(int[][] logs) {
 
         List<int[]> events=new ArrayList<>();//{year,event(either +1 or -1)}
@@ -36,4 +36,34 @@ class Solution {
 
         return year;
     }
+    
+
+    //Difference Array Technique
+    // public int maximumPopulation(int[][] logs) {
+
+    //     int[] years=new int[2051];//years maximum: 1950-2050 according to constraints
+
+
+    //     for(int[] log:logs){//marking birth year as +1  until death, death year as  -1
+    //         int birthYear=log[0],deathYear=log[1];
+    //         years[birthYear]+=1;
+    //         years[deathYear]-=1;
+    //     }
+
+    //     int currPopulation=0,maxPopulation=0,year=0;
+
+    //     //process based on years
+    //     for(int yr=1950;yr<=2050;yr++){
+
+    //         currPopulation+=year[yr];
+    //         //if current population on current year is greater than already maximum population,update it
+    //         if(currPopulation>maxPopulation){
+    //             maxPopulation=currPopulation;
+    //             year=currYear;
+    //         }
+    //     }
+
+    //     return year;
+    // }
+
 }
