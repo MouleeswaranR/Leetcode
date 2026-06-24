18e1ef5e664786273ee1dd931b2b5dd8f8989afc
MouleeswaranR <moulee078@gmail.com>
2026-06-24 15:57:34 +0530

    Time: 2 ms (90.69%) | Memory: 44.1 MB (71.78%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,52 +1,52 @@
-public class Pair{
-    int first;
-    int second;
-    int tm;
-    Pair(int first,int second,int tm){
-        this.first=first;
-        this.second=second;
-        this.tm=tm;
-    }
-}
 
+//Multi-Source BFS=>here all the rotten oranges are sources,need minimum time to rot all fresh oranges
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n=grid.length, m=grid[0].length;
-        int[][] vis=new int[n][m];
-        Queue<Pair> q=new LinkedList<>();
-        int cntFresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
-                    vis[i][j]=2;
-                }else{
-                    vis[i][j]=0;
+        int rows=grid.length,cols=grid[0].length;
+
+        Queue<int[]> queue=new LinkedList<>();
+        int freshCount=0;
+
+        for(int row=0;row<rows;row++){
+            for(int col=0;col<cols;col++){
+                //getting initial rotten oranges
+                if(grid[row][col]==2){
+                    queue.offer(new int[]{row,col});
+                }else if(grid[row][col]==1){
+                    freshCount++;//count initial fresh oranges
                 }
-                if(grid[i][j]==1) cntFresh++;
             }
         }
-        int tm=0;
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
-        int cnt=0;
-        while(!q.isEmpty()){
-            int r=q.peek().first;
-            int c=q.peek().second;
-            int t=q.peek().tm;
-            tm=Math.max(tm,t);
-            q.remove();
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-                    q.add(new Pair(nrow,ncol,t+1));
-                    vis[nrow][ncol]=2;
-                    cnt++;
-                }
-            }         
+
+        //if there are no fresh ornages to return, return 0
+        if(freshCount==0)return 0;
+
+        int[][] directions={{0,-1},{0,1},{-1,0},{1,0}};
+        int time=0;
+
+        while(!queue.isEmpty()){
+            int size=queue.size();
+
+            while(size-->0){
+                int[] currentCoordinates=queue.poll();
+
+                int currX=currentCoordinates[0],currY=currentCoordinates[1];
+
+                for(int[] direction:directions){
+                    int newX=currX+direction[0],newY=currY+direction[1];
+
+                    //check if new coordinate orange is fresh(equal to 1)
+                    if(newX>=0 && newX<rows && newY>=0 && newY<cols && grid[newX][newY]==1){
+                        grid[newX][newY]=2;//rot current orange
+                        queue.offer(new int[]{newX,newY});//push it into queue
+                        freshCount--;//reduce one freah orange as it is rotten
+                    }
+                }   
+               
+            }
+             time++;//increase the time for each level
         }
-        if(cntFresh!=cnt)return -1;
-        return tm;
+
+        return freshCount==0?time-1:-1;//if there are still fresh oranges,return -1
     }
 }
