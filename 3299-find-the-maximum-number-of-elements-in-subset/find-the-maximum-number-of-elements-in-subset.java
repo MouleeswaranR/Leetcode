5cd535d59e998f3f7be6f6fd18c9dc92f155a389
MouleeswaranR <moulee078@gmail.com>
2026-06-27 17:50:31 +0530

    Time: 64 ms (50.00%) | Memory: 71.1 MB (12.79%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -1,38 +1,35 @@
 class Solution {
     public int maximumLength(int[] nums) {
-        HashMap<Long, Long> freq = new HashMap<>();
+        Map<Long,Long> freq=new HashMap<>();
 
-        for (int x : nums) {
-            freq.put((long)x, freq.getOrDefault((long)x, 0L) + 1);
+        for(int num:nums){
+            freq.put((long)num,freq.getOrDefault((long)num,0L)+1);
         }
 
-        long maxLen = 0;
+        long result=0;
 
-        for (Map.Entry<Long, Long> x : freq.entrySet()) {
-            long curr = x.getKey();
+        for(Map.Entry<Long,Long> entry:freq.entrySet()){
+            long curr=entry.getKey();
 
-            if (curr == 1) {
-                maxLen = Math.max(maxLen, 
-                    freq.get(1L) % 2 == 0 ? freq.get(1L) - 1 : freq.get(1L));
+            if(curr==1){
+                result=Math.max(result,freq.get(1L)%2==0?freq.get(1L)-1:freq.get(1L));
                 continue;
             }
 
-            long currLen = 0;
-
-            while (freq.containsKey(curr) && freq.get(curr) >= 2) {
-                currLen += 2;
-                curr *= curr;
+            long currLen=0;
+            while(freq.containsKey(curr) && freq.get(curr)>=2){
+                currLen+=2;
+                curr*=curr;
             }
 
-            if (freq.containsKey(curr) && freq.get(curr) == 1) {
+            if(freq.containsKey(curr) && freq.get(curr)==1){
                 currLen++;
-            } else {
+            }else{
                 currLen--;
             }
-
-            maxLen = Math.max(maxLen, currLen);
+            result=Math.max(result,currLen);
         }
 
-        return (int) maxLen;
+        return (int)result;
     }
 }
